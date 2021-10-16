9a8588adf43f32d072470617fe74d64a3c1fc48f
Lisa Rozo <erozo190@gmail.com>
2021-10-16 11:58:59 -0500

    Created javascript rendered html page

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require("inquirer");
+const fs = require('fs');
+const generateHTML = require('./src/Myteam');
 
 function startApp(){
     inquirer.prompt([
@@ -38,28 +40,63 @@ function mainMenu(){
         }
     ]).then(({choice}) => {
         if(choice == "Engineer"){
+            
             console.log("you chose engineer")
             engineerQuestion()
         }
+        if(choice == "Intern"){
+            
+            console.log("you chose Intern")
+            internQuestions()
+        }
     })
+    
 }
 
 function engineerQuestion(){
     inquirer.prompt([
         {
             type: "input",
-            name: "mgrName",
-            message: "What is your Manager's name?"
+            name: "engineerName",
+            message: "What is your Engineer's name?"
         },
         {
             type: "input",
-            name: "mgrId",
-            message: "What is your Manager's Employee ID?"
+            name: "engineerId",
+            message: "What is your Engineer's ID?"
         },
         {
             type: "input",
-            name: "mgrEmail",
-            message: "What is your Manager's email?"
+            name: "engineerEmail",
+            message: "What is your Engineer's email?"
+        },
+        {
+            type: "input",
+            name: "engineerGithubUsername",
+            message: "What is your Engineer's Github username?"
+        },
+    ]).then(function(answers) {
+        console.log(answers)
+        mainMenu()
+    })
+}
+
+function internQuestions(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is your Intern's name?"
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "What is your 's Intern ID?"
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is your Intern's email?"
         },
         {
             type: "input",
@@ -71,4 +108,12 @@ function engineerQuestion(){
         mainMenu()
     })
 }
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, data, (err) => {
+          if (err) throw err;
+        });
+  }
+  writeToFile('myTeam.html', generateHTML() )
+
 startApp()
